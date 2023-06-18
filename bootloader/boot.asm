045bd9e04b8f7b8329bbfe7bc9c98ca47b7ca9f2
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-06-18 18:16:21 -0300

    Update boot.asm

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,1 +1,16 @@
+ORG 0x0000
 
+START:
+    LD SP, 0xFFFF
+
+    ; Setting up the video chip TMS9918A
+
+    LD HL, 0x8000
+    LD DE, 0x0000
+    LD BC, 0x4000
+
+    ; Looping
+
+    JR $
+
+    END START
