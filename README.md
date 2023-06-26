3f770b119d2ab953cac6fc4bb38d4b1f7a44490e
marcopereirajunior38@gmail.com <marcopereirajunior38@gmail.com>
2023-06-26 01:28:15 -0300

    lol

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # RetroOS
 your best operactional system for the Zilog Z80 processor open source
 
+# Secure, Fast and the best, Free
+are you searching for a OS secure and free? the RetroOS is the best option
+
 # Unleash your creativity and create mods
 ![](https://github.com/PaneladaDe87/RetroOS/blob/bf9a21e3fbe9c03cdbe4e6e1c52e9efbe4acd54d/RetroOS.png)
--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,26 +1,19 @@
 ORG 0x0000
 
 START:
-    LD SP, 0xFFFF
-
-    ; Setting up the video chip TMS9918A
-    LD HL, 0x8000
-    LD DE, 0x0000
-    LD BC, 0x4000
-
-    ; Commands for boot(just a idiot test)
-    LD A, 0x1024
-    ADD A, B
-
-    ; Infinite loop
-    CP A
-    JR Z, SETUP
-    JR TEST
-
-    END START
+    LD SP, 0xFF00
+    LD A, (0)
+    
+    JP SETUP
 
 SETUP:
     DB "Your machine is booting, please, wait"
+    
+    ADD A, (1)
+    
+    CP A
+    JR NZ, INIT
 
-TEST:
-    DB "Here a test screen"
+INIT:
+    DB "Your machine has been booted sucessfuly"
+    DB "Going to main screen, please wait..."
